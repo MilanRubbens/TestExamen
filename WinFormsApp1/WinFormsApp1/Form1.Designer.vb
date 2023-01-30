cdbec797643afa9715ca7eb66cee1a03b6763899
Milan Rubbens <milan.rubbens7@gmail.com>
2023-01-30 19:52:50 +0100

    Testexamen

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(89, 561)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(267, 46)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Geld op zichtrekening"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 47)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(83, 32)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(89, 100)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 32)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(89, 155)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(83, 32)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(89, 208)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(83, 32)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(514, 47)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(83, 32)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(514, 100)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 32)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(514, 155)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(83, 32)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(514, 208)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(83, 32)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "Label8"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(514, 265)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(83, 32)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "Label9"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(383, 561)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(500, 46)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "Geld van zichtrekening naar spaarrekening"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(383, 633)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(500, 46)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Geld van spaarrekening naar zichtrekening"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(964, 561)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(150, 46)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Dollar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(964, 633)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(150, 46)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "Pond"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(964, 705)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(150, 46)
+        Me.Button6.TabIndex = 14
+        Me.Button6.Text = "Euro"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(89, 633)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(267, 46)
+        Me.Button7.TabIndex = 15
+        Me.Button7.Text = "Vraag een transactie op"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(484, 705)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(299, 46)
+        Me.Button8.TabIndex = 16
+        Me.Button8.Text = "Encrypt rekeningnummer"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(964, 47)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(150, 46)
+        Me.Button9.TabIndex = 17
+        Me.Button9.Text = "Rente 2024"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(964, 114)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(150, 46)
+        Me.Button10.TabIndex = 18
+        Me.Button10.Text = "Rente 2025"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(964, 179)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(150, 46)
+        Me.Button11.TabIndex = 19
+        Me.Button11.Text = "Rente 2026"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(536, 498)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(199, 46)
+        Me.Button12.TabIndex = 20
+        Me.Button12.Text = "Toon transactie"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(536, 428)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(203, 46)
+        Me.Button13.TabIndex = 21
+        Me.Button13.Text = "Vraag transactie"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 32.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1126, 804)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,187 @@
+﻿Imports System.Reflection.Emit
+
+Public Class Form1
+    Dim voornaam As String
+    Dim achternaam As String
+    Dim gender As String
+    Dim rekeningnummer As String
+    Dim zichtrekening As Double
+    Dim spaarrekening As Double
+    Dim transacties() As String = {""}
+    Dim i As Integer = 0
+    Dim encryptie As Boolean = True
+    Dim rente2024 As Double
+    Dim rente2025 As Double
+    Dim rente2026 As Double
+    Dim aantal
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        voornaam = InputBox("Geef je voornaam in").ToUpper
+        achternaam = InputBox("Geef je achternaam in").ToUpper
+        gender = InputBox("Geef je gender in M/V/A").ToUpper
+        rekeningnummer = InputBox("Geef je rekeningnummer in xx xxxx xxxx xxxx")
+        Label1.Text = "Voornaam: " & voornaam
+        Label2.Text = "Achternaam: " & achternaam
+        Label3.Text = "Gender: " & gender
+        Label4.Text = "Rekeningnummer: BE" & rekeningnummer
+        Label5.Text = zichtrekening
+        Label6.Text = spaarrekening
+        Label7.Text = "Euro"
+        Label8.Text = "Transacties"
+        Label9.Text = "Huidige rentevoet 1,35%"
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If zichtrekening >= 250 Then
+            MsgBox("Zichtrekening kan maar maximum tot 250 gaan")
+            Label5.Text = zichtrekening
+        Else
+            Dim Geldstorten = InputBox("Hoeveel geld wil je storen?")
+            If Geldstorten > 250 Then
+                MsgBox("Dit bedrag is te hoog")
+                Geldstorten = InputBox("Hoeveel geld wil je storen?")
+                zichtrekening += Geldstorten
+                For i As Integer = 0 To transacties.Length - 1
+                    transacties(i) += Geldstorten
+
+                    Array.Resize(transacties, transacties.Length + 1)
+                    aantal = String.Join(" ", transacties)
+                Next
+            Else
+                zichtrekening += Geldstorten
+                For i As Integer = 0 To transacties.Length - 1
+                    transacties(i) += Geldstorten
+
+                    Array.Resize(transacties, transacties.Length + 1)
+                    aantal = String.Join(" ", transacties)
+                Next
+
+            End If
+            Label5.Text = zichtrekening
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim overzetten = InputBox("Hoeveel geld wil je overzetten?")
+        If zichtrekening > overzetten Then
+            zichtrekening -= overzetten
+            spaarrekening += overzetten
+            Label5.Text = zichtrekening
+            Label6.Text = spaarrekening
+        Else
+            MsgBox("Je hebt niet genoeg geld op je zichtrekening")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim overzetten = InputBox("Hoeveel geld wil je overzetten?")
+        If spaarrekening > overzetten Then
+            zichtrekening += overzetten
+            spaarrekening -= overzetten
+            Label5.Text = zichtrekening
+            Label6.Text = spaarrekening
+        Else
+            MsgBox("Je hebt niet genoeg geld op je zichtrekening")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        If Label7.Text = "Euro" Then
+            zichtrekening = zichtrekening * 1.0887
+            spaarrekening = spaarrekening * 1.0887
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Dollar"
+        ElseIf Label7.Text = "Pond" Then
+            zichtrekening = zichtrekening * 1.0881
+            spaarrekening = spaarrekening * 1.0881
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Dollar"
+        End If
+
+
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Label7.Text = "Euro" Then
+            zichtrekening = zichtrekening * 0.8798
+            spaarrekening = spaarrekening * 0.8798
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Pond"
+        ElseIf Label7.Text = "Dollar" Then
+            zichtrekening = zichtrekening * 0.919
+            spaarrekening = spaarrekening * 0.919
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Pond"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If Label7.Text = "Dollar" Then
+            zichtrekening = zichtrekening * 0.9185
+            spaarrekening = spaarrekening * 0.9185
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Euro"
+        ElseIf Label7.Text = "Pond" Then
+            zichtrekening = zichtrekening * 1.1371
+            spaarrekening = spaarrekening * 1.1371
+            Label5.Text = Math.Round(zichtrekening)
+            Label6.Text = Math.Round(spaarrekening)
+            Label7.Text = "Euro"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If encryptie = True Then
+            Label4.Text = ("Rekeningnummer " & "BEXX XXXX XXXX XXXX")
+            encryptie = False
+        Else
+
+            Label4.Text = ("Rekeningnummer: " & rekeningnummer)
+            encryptie = True
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        rente2024 = spaarrekening * 1.35
+        Label9.Text = ("Interest voor 2024 " + Math.Round(rente2024, 2))
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        rente2025 = (spaarrekening * 1.35) * 2
+        Label9.Text = ("Interest voor 2025 " + Math.Round(rente2025, 2))
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        rente2026 = (spaarrekening * 1.35) * 3
+        Label9.Text = ("Interest voor 2026 " + Math.Round(rente2026, 2))
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+
+        Dim aantal = String.Join(" ", transacties)
+        Label8.Text = (" " & aantal)
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        Dim vraagTrans = InputBox("Geef een transactie die je wilt bekijken")
+        MsgBox("De transactie is " + transacties(vraagTrans))
+    End Sub
+
+    Private Sub Button5_Click_1(sender As Object, e As EventArgs) Handles Button5.Click
+
+    End Sub
+End Class
